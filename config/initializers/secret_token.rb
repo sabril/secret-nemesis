32460580ccc456f5e42ee00f53f60cc822c4a4b7
Andrew Nicol <a.k.nicol@gmail.com>
2012-06-22 19:58:45 -0400

    New

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Patentlookup::Application.config.secret_token = 'a946eecc630b127214d77dfd1e9b3d76425922b36b61574047e9fa846ee36e2cd266f459ae056119807df7c2c0d82f82aae38e65e44b4278daefb6ee2d4f631a'

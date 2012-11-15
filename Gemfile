8dd4c79963e15a98a56a8f870a70a85cc4241d1b
Andrew Nicol <a.k.nicol@gmail.com>
2012-11-15 00:11:50 -0500

    New

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rake'
-gem 'rails', '3.2.0'
+gem 'rails', '3.2.8'
 gem 'mysql2'
 gem 'json'
 gem 'jquery-rails'
--- app/controllers/patents_controller.rb
+++ app/controllers/patents_controller.rb
@@ -1,6 +1,9 @@
 class PatentsController < ApplicationController
   
   def index
+    if !params[:browse]
+      params[:browse]="all"
+    end
     if params[:browse]!="all"
       @patents = Patent.where("application_type=?",params[:browse]).order("filing_date DESC").page(params[:page]).per(50)
     else

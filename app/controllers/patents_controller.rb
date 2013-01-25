557b1c2179f29cea4355a9a071ac5352aaac829f
artdvl <artdvl91@gmail.com>
2013-01-26 05:48:12 +0700

    fix

--- app/controllers/patents_controller.rb
+++ app/controllers/patents_controller.rb
@@ -54,17 +54,13 @@ class PatentsController < ApplicationController
           search_query << " @application_status #{params[:patent][:application_status]}" if content != [""]
           next
         end
-        # if column == "created_at"
-        #                           search_query << " @filing_date #{2.year.ago..Time.now}" if content != ""
-        #                           next
-        #                         end
       end
+      
       if params[:patent][:created_at] != "" and params[:patent][:updated_at] != ""
         @patents = Patent.search(search_query, :with => {:filing_date => Date.parse(params[:patent][:created_at].gsub(/, */, '-') )..Date.parse(params[:patent][:updated_at].gsub(/, */, '-') )}, :match_mode => :extended, :ignore_errors => true, :order => sort_column, :sort_mode => sort_direction).page(params[:page])
       else
         @patents = Patent.search(search_query, :match_mode => :extended, :ignore_errors => true, :order => sort_column, :sort_mode => sort_direction).page(params[:page])
       end
-      # @patents = Patent.search(:conditions => {:application_type => params[:patent][:application_type], :application_status => params[:patent][:application_status]}, :ignore_errors => true, :order => sort_column, :sort_mode => sort_direction).page(params[:page])
       
     else
       @patents_search = Patent.new

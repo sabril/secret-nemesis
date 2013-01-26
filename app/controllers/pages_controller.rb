2f5a0bdb9eadbf4528523bb6470ea2a70e86bd30
artdvl <artdvl91@gmail.com>
2013-01-26 18:57:56 +0700

    fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,8 @@ class PagesController < ApplicationController
   def home
     @title = "Patent Lookup Australia"
     @meta_description = "Robust search of over 1 million Australian patents, including provisional patents."
+    @patents_new = Patent.search(:order => :created_at, :page => 1, :per_page => 10)
+    @patents_trend = Patent.search(:order => "@random ASC", :page => 1, :per_page => 10)
   end
 
 end
